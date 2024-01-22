9c33dda4519bf860b47489049ede85afc7140576
Yithzabi Aridai F. Perez <141900042+Aridai-Perez@users.noreply.github.com>
2024-01-22 14:34:29 -0800

    Update calculator.test.js

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -3,22 +3,23 @@ const { default: expect } = require('expect');
 const calculator = require('../calculator');
 
 test('adds a + b', () => {
-    //
+    expect(calculator.sum(5,2)).toBe(7);
 })
 
 test('substracts a - b', () => {
-   //
+    expect(calculator.substract(10,6)).toBe(4);
 })
 
 test('multiplies a * b', () => {
-    //
+    expect(calculator.multiply(5,4)).toBe(20)
 })
 
 test('divides a / b', () => {
-    // Consider decimals
+    expect(calculator.divide(10,2)).toBe(5);
+
 })
 
 test('Raises to power a ^ b', () => {
-    //
+    expect(calculator.power(3,4)).toBe(81)
 })
 
