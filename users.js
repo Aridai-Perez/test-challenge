8246a19d43023c6eb3cfb94bba10ef054b31261b
Yithzabi Aridai F. Perez <141900042+Aridai-Perez@users.noreply.github.com>
2024-01-22 14:31:02 -0800

    ejercicios resueltos

--- users.js
+++ users.js
@@ -3,17 +3,18 @@
 const users = require('./data');
 
 // Fetches all users
-const getUsers = () => {
-    return users;
-}
+// const getUsers = () => {
+//     return users;
+// }
 
 // Filters users by specific ID
-const getUser = id => {
-    return users.find(user => user.id === id)
+function getUserById(id) {
+    return users.find(user => user.id === id);
 }
+
 
 
 // test
 // console.log(getUser(3));
 
-module.exports = {getUsers , getUser};
+module.exports = getUserById;
